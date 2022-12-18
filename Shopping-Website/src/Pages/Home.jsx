256f29a2f895cdb649c82f202b14339ad186b826
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2022-12-18 19:53:14 +0530

    api setup

--- Shopping-Website/src/Pages/Home.jsx
+++ Shopping-Website/src/Pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
 
-    const [product, setproduct] = useState([])
+  const [product, setproduct] = useState([]);
 
+  useEffect(() => {
+
+    const productList = async () => {
+      const data = await fetch('https://fakestoreapi.com/products/1')
+      const result = data.json();
+      setproduct(result);
+    };
+  
+    return () => {
+      productList();
+    }
+
+  }, [])
+  
   return (
     <pre>{JSON.stringify(product, null, 2)}</pre>
   )
